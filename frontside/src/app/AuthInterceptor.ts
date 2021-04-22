63aecf231c2bff3096c684391ce552c660d92f9d
Vlmas <aalmas2001@gmail.com>
2021-04-22 15:58:57 +0600

    final state

--- frontside/src/app/AuthInterceptor.ts
+++ frontside/src/app/AuthInterceptor.ts
@@ -1,16 +1,16 @@
 import { HttpHandler, HttpInterceptor, HttpEvent, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = localStorage.getItem('token')
         if(token) {
-            const newRequst = req.clone({
+            const newRequest = req.clone({
                 headers: req.headers.append('Authorization', 'JWT ' + token)
             });
-            return next.handle(newRequst); 
+            return next.handle(newRequest); 
         }
         return next.handle(req);
     }
